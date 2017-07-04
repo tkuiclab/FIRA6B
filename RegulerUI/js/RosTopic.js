a4aec57cc3939ca3c0dc05061d061ed5e297aa30
minda93 <wwe421210w@gmail.com>
2017-07-04 16:57:34 +0800

    Update rostopic remote

--- RegulerUI/js/RosTopic.js
+++ RegulerUI/js/RosTopic.js
@@ -2,28 +2,28 @@
 //MotionRemote
 var Remote1 = new ROSLIB.Topic({
     ros: ros,
-    name: '/motion/Remote',
-    messageType: 'std_msgs/Int32'
+    name: '/motion/remote',
+    messageType: 'std_msgs/Bool'
 });
 var Remote2 = new ROSLIB.Topic({
     ros: ros2,
-    name: '/motion/Remote',
-    messageType: 'std_msgs/Int32'
+    name: '/motion/remote',
+    messageType: 'std_msgs/Bool'
 });
 var Remote3 = new ROSLIB.Topic({
     ros: ros3,
-    name: '/motion/Remote',
-    messageType: 'std_msgs/Int32'
+    name: '/motion/remote',
+    messageType: 'std_msgs/Bool'
 });
 function RemoteSwitch(state){
     var check;
 	if(state){
-		RemoteState = parseInt(1);
+		RemoteState = true;
         check = new ROSLIB.Message({
             data: RemoteState
         });
     }else{
-		RemoteState = parseInt(0);
+		RemoteState = false;
         check = new ROSLIB.Message({
             data: RemoteState
         });
