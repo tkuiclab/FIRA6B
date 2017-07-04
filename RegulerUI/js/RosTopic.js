2d9ceb612ae75d72ce91be7302f03375be069d08
minda93 <wwe421210w@gmail.com>
2017-07-04 17:05:40 +0800

    Update rostopic.js shoot

--- RegulerUI/js/RosTopic.js
+++ RegulerUI/js/RosTopic.js
@@ -171,17 +171,17 @@ function PublishTopicCmdVel(vec3) {
 //shoot
 var TopicShoot1 = new ROSLIB.Topic({
     ros: ros,
-    name: '/motion//shoot',
+    name: '/motion/shoot',
     messageType: 'std_msgs/Int32'
 });
 var TopicShoot2 = new ROSLIB.Topic({
     ros: ros2,
-    name: '/motion//shoot',
+    name: '/motion/shoot',
     messageType: 'std_msgs/Int32'
 });
 var TopicShoot3 = new ROSLIB.Topic({
     ros: ros3,
-    name: '/motion//shoot',
+    name: '/motion/shoot',
     messageType: 'std_msgs/Int32'
 });
 
