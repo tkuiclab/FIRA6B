c7ecface168f340f21bc9f55063c410c3b65fc3e
leokim0506 <k0910840540@gmail.com>
2017-08-02 17:50:38 +0800

    new aim point

---
+++ src/vision/Interface/main.cpp
@@ -0,0 +1,16 @@
+#include <QtGui/QApplication>
+#include "interface_window.hpp"
+#include "qInterface.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char *argv[])
+{
+    QInterface m(argc,argv);
+    QApplication a(argc, argv);
+    
+    interface_window w(&m);
+    w.show();
+
+    return a.exec();
+}
